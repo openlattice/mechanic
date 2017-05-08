c39cc397037e73bdfd47359d44b54afc9fb1b8d5
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-05-07 23:11:25 -0700

    Fix prepare

--- src/main/java/com/dataloom/mechanic/upgrades/EdgeTypeMigrator.java
+++ src/main/java/com/dataloom/mechanic/upgrades/EdgeTypeMigrator.java
@@ -4,6 +4,7 @@ import com.dataloom.streams.StreamUtil;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.kryptnostic.datastore.cassandra.CommonColumns;
 import com.kryptnostic.datastore.cassandra.RowAdapters;
@@ -13,19 +14,19 @@ import com.kryptnostic.datastore.cassandra.RowAdapters;
  */
 public class EdgeTypeMigrator {
     private final Session           session;
-    private final PreparedStatement readEntityTypes;
+    private final Statement         readEntityTypes;
     private final PreparedStatement writeEntityTypes;
 
     public EdgeTypeMigrator(
             Session session,
             String keyspace ) {
         this.session = session;
-        this.readEntityTypes = session
-                .prepare( QueryBuilder.select( "id", "category" )
+        this.readEntityTypes =
+                QueryBuilder.select( "id", "category" )
                         .from( keyspace, "entity_types" )
                         .allowFiltering()
                         .where(
-                                QueryBuilder.eq( CommonColumns.CATEGORY.cql(), "EdgeType" ) ) );
+                                QueryBuilder.eq( CommonColumns.CATEGORY.cql(), "EdgeType" ) );
         this.writeEntityTypes = session
                 .prepare( QueryBuilder.update( keyspace, "entity_types" )
                         .where( CommonColumns.ID.eq() )
@@ -34,7 +35,7 @@ public class EdgeTypeMigrator {
     }
 
     public long upgrade() {
-        return StreamUtil.stream( session.execute( readEntityTypes.bind() ) )
+        return StreamUtil.stream( session.execute( readEntityTypes ) )
                 .parallel()
                 .map( RowAdapters::id )
                 .map( id -> session.executeAsync( writeEntityTypes.bind().setUUID( CommonColumns.ID.cql(), id ) ) )
