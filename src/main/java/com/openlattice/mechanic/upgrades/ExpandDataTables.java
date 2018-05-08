d1d469e7b35835694dbae7f707218107d97970b1
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-05-08 09:36:09 -0700

    Streamline data migration

--- src/main/java/com/openlattice/mechanic/upgrades/ExpandDataTables.java
+++ src/main/java/com/openlattice/mechanic/upgrades/ExpandDataTables.java
@@ -22,13 +22,11 @@
 package com.openlattice.mechanic.upgrades;
 
 import com.dataloom.mappers.ObjectMappers;
-import com.dataloom.streams.StreamUtil;
 import com.google.common.base.Charsets;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Queues;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.openlattice.data.EntityDataKey;
 import com.openlattice.data.EntityDataMetadata;
@@ -38,24 +36,28 @@ import com.openlattice.data.mapstores.PostgresDataMapstore;
 import com.openlattice.datastore.cassandra.CassandraSerDesFactory;
 import com.openlattice.edm.EntitySet;
 import com.openlattice.edm.PostgresEdmManager;
-import com.openlattice.edm.type.EntityType;
 import com.openlattice.edm.type.PropertyType;
 import com.openlattice.postgres.DataTables;
+import com.openlattice.postgres.ResultSetAdapters;
 import com.openlattice.postgres.mapstores.EntitySetMapstore;
 import com.openlattice.postgres.mapstores.EntityTypeMapstore;
 import com.openlattice.postgres.mapstores.PropertyTypeMapstore;
 import com.openlattice.postgres.mapstores.data.DataMapstoreProxy;
 import com.openlattice.postgres.mapstores.data.EntityDataMapstore;
 import com.openlattice.postgres.mapstores.data.PropertyDataMapstore;
+import com.zaxxer.hikari.HikariDataSource;
 import java.nio.ByteBuffer;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
@@ -78,9 +80,11 @@ public class ExpandDataTables {
     private final PropertyTypeMapstore     ptm;
     private final EntityTypeMapstore       etm;
     private final EntitySetMapstore        esm;
+    private final HikariDataSource         hds;
     private final ListeningExecutorService executorService;
 
     public ExpandDataTables(
+            HikariDataSource hds,
             PostgresDataMapstore dataMapstore,
             DataMapstoreProxy dmProxy,
             PostgresEdmManager pgEdmManager,
@@ -88,6 +92,7 @@ public class ExpandDataTables {
             EntityTypeMapstore etm,
             EntitySetMapstore esm,
             ListeningExecutorService executorService ) {
+        this.hds = hds;
         this.dataMapstore = dataMapstore;
         this.dmProxy = dmProxy;
         this.pgEdmManager = pgEdmManager;
@@ -111,7 +116,7 @@ public class ExpandDataTables {
         propertyTypes.stream()
                 .map( PropertyType::getId )
                 .peek( table -> logger.info( "Creating table for property type id {}", table ) )
-                .forEach( dmProxy::getPropertyMapstore);
+                .forEach( dmProxy::getPropertyMapstore );
 
         for ( EntitySet es : esm.loadAll( Lists.newArrayList( esm.loadAllKeys() ) ).values() ) {
             logger.info( "Starting table creation for entity set: ", es.getName() );
@@ -133,83 +138,115 @@ public class ExpandDataTables {
         PropertyMetadata pm = PropertyMetadata.newPropertyMetadata( OffsetDateTime.now() );
         logger.info( "Starting migration of data keys." );
         final AtomicLong migratedCount = new AtomicLong( 0 );
-        final BlockingQueue<DataKey> dataKeys = Queues.newArrayBlockingQueue( 23000000 );
+        final List<OldPostgresData> oldData = new ArrayList<>( 24000000 );
+        final List<UUID> syncIds = new ArrayList<>( 400 );
         final AtomicLong loadedCount = new AtomicLong();
         final AtomicBoolean loading = new AtomicBoolean( true );
 
-        executorService.execute( () -> {
-            dataMapstore.loadAllKeys().forEach( dataKey -> {
-                dataKeys.add( dataKey );
-                loadedCount.incrementAndGet();
-            } );
-            logger.info( "Loaded {} keys into blocking queue!", loadedCount.get() );
-            loading.set( false );
+        final String syncIdQuery = "SELECT DISTINCT current_sync_id FROM sync_ids";
+
+        try ( final Connection connection = hds.getConnection();
+                final Statement stmt = connection.createStatement();
+                final ResultSet rs = stmt.executeQuery( syncIdQuery ) ) {
+            while ( rs.next() ) {
+                syncIds.add( (UUID) rs.getObject( "current_sync_ids" ) );
+            }
+        } catch ( SQLException e ) {
+            logger.error( "Unable to query for sync ids" );
+        }
+
+        final String sqlQuery = "select * from data where syncid = ?";
+
+        logger.info( "Starting load of data into memory." );
+
+        Stopwatch w = Stopwatch.createStarted();
+
+        syncIds.parallelStream().forEach( syncId -> {
+            try ( final Connection connection = hds.getConnection();
+                    final PreparedStatement ps = connection.prepareStatement( sqlQuery ) ) {
+                ps.setObject( 1, syncId );
+                try ( ResultSet rs = ps.executeQuery() ) {
+                    while ( rs.next() ) {
+                        oldData.add( new OldPostgresData(
+                                ResultSetAdapters.dataKey( rs ),
+                                rs.getBytes( "property_buffer" ) ) );
+                    }
+                }
+            } catch ( SQLException e ) {
+                logger.error( "Unable to query for data for syncid {}", syncId );
+            }
         } );
 
-        try {
-            Thread.sleep( 2000 );
-        } catch ( InterruptedException e ) {
-            logger.warn( "Unable to sleep." );
-        }
+        logger.info( "Finished load of data into memory in {} ms", w.elapsed( TimeUnit.MILLISECONDS ) );
 
-        final int numProcs = Runtime.getRuntime().availableProcessors() - 1;
-        final List<ListenableFuture> futures = new ArrayList<>( numProcs );
+        w.reset();
 
-        for ( int i = 0; i < numProcs; ++i ) {
-            futures.add( executorService.submit( () -> {
-                try {
-                    for ( DataKey dataKey = dataKeys.take();
-                            loading.get() || !dataKeys.isEmpty();
-                            dataKey = dataKeys.poll( 5, TimeUnit.MINUTES ) ) {
+        logger.info( "Starting migration of {} keys.", oldData.size() );
+        w.start();
+        oldData.parallelStream().forEach( oldDatum -> {
+            final DataKey dataKey = oldDatum.getDataKey();
 
-                        if ( ( migratedCount.incrementAndGet() % 10000 ) == 0 ) {
-                            logger.info( "Migrated {} keys.", migratedCount );
-                        }
-                        final EntityDataMapstore entityDataMapstore = dmProxy
-                                .getMapstore( dataKey.getEntitySetId() );
-                        final PropertyDataMapstore pdm = dmProxy
-                                .getPropertyMapstore( dataKey.getPropertyTypeId() );
-                        final UUID entitySetId = dataKey.getEntitySetId();
-                        final UUID entityKeyId = dataKey.getId();
-                        entityDataMapstore
-                                .store( entityKeyId,
-                                        EntityDataMetadata.newEntityDataMetadata( OffsetDateTime.now() ) );
-                        ByteBuffer buffer = dataMapstore.load( dataKey );
-                        final Object obj;
-                        try {
-                            if ( dataKey.getPropertyTypeId().equals( complaintNumberPropertyTypeId )
-                                    && buffer.array().length == 8 ) {
-                                logger.info(
-                                        "Detected complaint number that is a long-- switching to alternate deserialization" );
-                                obj = CassandraSerDesFactory.deserializeValue( ObjectMappers.getJsonMapper(),
-                                        buffer,
-                                        EdmPrimitiveTypeKind.Int64,
-                                        dataKey.getEntityId() ).toString();
-                            } else {
-                                obj = CassandraSerDesFactory.deserializeValue( ObjectMappers.getJsonMapper(),
-                                        buffer,
-                                        ptm.load( dataKey.getPropertyTypeId() ).getDatatype(),
-                                        dataKey.getEntityId() );
-                            }
-
-                            pdm.store( new EntityDataKey( entitySetId, entityKeyId ), ImmutableMap.of( obj, pm ) );
-                        } catch ( Exception edp ) {
-                            logger.error(
-                                    "Unable to process entity with id {} in entity set {} with property type {} and value {}",
-                                    entityKeyId,
-                                    entitySetId,
-                                    dataKey.getPropertyTypeId(),
-                                    new String( buffer.array(), Charsets.UTF_8 ),
-                                    edp );
-                        }
-
-                    }
-                } catch ( InterruptedException e ) {
-                    logger.error( "Unable to read data key from queue." );
+            if ( ( migratedCount.incrementAndGet() % 10000 ) == 0 ) {
+                logger.info( "Migrated {} keys.", migratedCount );
+            }
+            final EntityDataMapstore entityDataMapstore = dmProxy
+                    .getMapstore( dataKey.getEntitySetId() );
+            final PropertyDataMapstore pdm = dmProxy
+                    .getPropertyMapstore( dataKey.getPropertyTypeId() );
+            final UUID entitySetId = dataKey.getEntitySetId();
+            final UUID entityKeyId = dataKey.getId();
+            entityDataMapstore
+                    .store( entityKeyId,
+                            EntityDataMetadata.newEntityDataMetadata( OffsetDateTime.now() ) );
+            ByteBuffer buffer = ByteBuffer.wrap( oldDatum.getData() );
+            final Object obj;
+            try {
+                if ( dataKey.getPropertyTypeId().equals( complaintNumberPropertyTypeId )
+                        && buffer.array().length == 8 ) {
+                    logger.info(
+                            "Detected complaint number that is a long-- switching to alternate deserialization" );
+                    obj = CassandraSerDesFactory.deserializeValue( ObjectMappers.getJsonMapper(),
+                            buffer,
+                            EdmPrimitiveTypeKind.Int64,
+                            dataKey.getEntityId() ).toString();
+                } else {
+                    obj = CassandraSerDesFactory.deserializeValue( ObjectMappers.getJsonMapper(),
+                            buffer,
+                            ptm.load( dataKey.getPropertyTypeId() ).getDatatype(),
+                            dataKey.getEntityId() );
                 }
-            } ) );
+
+                pdm.store( new EntityDataKey( entitySetId, entityKeyId ), ImmutableMap.of( obj, pm ) );
+            } catch ( Exception edp ) {
+                logger.error(
+                        "Unable to process entity with id {} in entity set {} with property type {} and value {}",
+                        entityKeyId,
+                        entitySetId,
+                        dataKey.getPropertyTypeId(),
+                        new String( buffer.array(), Charsets.UTF_8 ),
+                        edp );
+            }
+        } );
+        logger.info( "Successfully migrated {} keys in {} seconds.",
+                migratedCount.get(),
+                w.elapsed( TimeUnit.SECONDS ) );
+    }
+
+    static class OldPostgresData {
+        private final DataKey dataKey;
+        private final byte[]  data;
+
+        OldPostgresData( DataKey dataKey, byte[] data ) {
+            this.dataKey = dataKey;
+            this.data = data;
         }
-        futures.forEach( StreamUtil::getUninterruptibly );
-        logger.info( "Finish migration of data keys." );
+
+        public DataKey getDataKey() {
+            return dataKey;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
     }
 }
