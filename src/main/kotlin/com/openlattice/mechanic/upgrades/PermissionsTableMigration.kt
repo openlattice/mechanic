c0a21b5bdf2294b75c4c39e6c3446f30901c5d82
Piper Keyes <pckeyes@stanford.edu>
2018-09-20 15:24:56 -0700

    migrator to add expiration date column to permissions table

---
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/PermissionsTableMigration.kt
@@ -0,0 +1,19 @@
+package com.openlattice.mechanic.upgrades
+
+import com.google.common.util.concurrent.ListeningExecutorService
+import com.zaxxer.hikari.HikariDataSource
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger(Organizations::class.java)
+
+class PermissionsTableMigration(
+        private val hds: HikariDataSource,
+        private val executor: ListeningExecutorService
+) {
+
+    fun addExpirationDateCol() {
+        hds.connection.use {
+            it.createStatement().execute("alter table permissions add column expiration_date timestamp with time zone;")
+        }
+    }
+}
