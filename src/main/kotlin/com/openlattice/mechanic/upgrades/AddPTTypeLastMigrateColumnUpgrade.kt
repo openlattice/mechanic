fca85d6d14c2ee7f2c37626fafa6571664ceb552
Drew <drew@openlattice.com>
2019-06-14 21:13:38 -0400

    slightly more sane?

--- src/main/kotlin/com/openlattice/mechanic/upgrades/AddPTTypeLastMigrateColumnUpgrade.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/AddPTTypeLastMigrateColumnUpgrade.kt
@@ -1,6 +1,11 @@
 package com.openlattice.mechanic.upgrades
 
 import com.openlattice.mechanic.Toolbox
+import com.openlattice.postgres.streams.PostgresIterable
+import com.openlattice.postgres.streams.StatementHolder
+import java.sql.ResultSet
+import java.util.function.Function
+import java.util.function.Supplier
 
 class AddPTTypeLastMigrateColumnUpgrade(private val toolbox: Toolbox) : Upgrade {
 
@@ -28,16 +33,20 @@ class AddPTTypeLastMigrateColumnUpgrade(private val toolbox: Toolbox) : Upgrade 
             toolbox.propertyTypes.entries.forEach {propertyEntry ->
                 val propertyId = propertyEntry.key
                 // select rows to migrate
-                conn.createStatement().use { stmt ->
-                    stmt.executeQuery(
-                        "SELECT * FROM pt_$propertyId WHERE last_migrate < last_write"
-                    ).use {
-                        // migrate rows
-                        conn.prepareStatement(INSERT_SQL).use{ ps ->
-                            ps.setObject(0, Object())
-                        }
+                val toMigrate = PostgresIterable(Supplier {
+                    conn.createStatement().use { stmt ->
+                        val rs = stmt.executeQuery(
+                                "SELECT * FROM pt_$propertyId WHERE last_migrate < last_write"
+                        )
+                        StatementHolder(conn, stmt, rs)
                     }
-                }
+                }, Function<ResultSet, Unit> {
+                    conn.prepareStatement(INSERT_SQL).use{ ps ->
+                        // do le settings here
+                        ps.execute()
+                    }
+                } )
+                // migrate rows
                 conn.commit()
             }
         }
@@ -48,7 +57,7 @@ class AddPTTypeLastMigrateColumnUpgrade(private val toolbox: Toolbox) : Upgrade 
     val cols = arrayOf("entity_set_id", "id", "partition", "property_type_id", "hash",
             "b_TEXT", "b_UUID", "b_SMALLINT", "b_INTEGER", "b_BIGINT", "b_DATE", "b_TIMESTAMPTZ", "b_DOUBLEPRECISION", "b_BOOLEAN",
             "g_TEXT", "g_UUID", "g_SMALLINT", "g_INTEGER", "g_BIGINT", "g_DATE", "g_TIMESTAMPTZ", "g_DOUBLEPRECISION", "g_BOOLEAN",
-            "n_TEXT", "n_UUID", "n_SMALLINT", "n_INTEGER", "n_BIGINT", "n_DATE", "n_TIMESTAMPTZ", "n_DOUBLEPRECISION", "n_BOOLEA"
+            "n_TEXT", "n_UUID", "n_SMALLINT", "n_INTEGER", "n_BIGINT", "n_DATE", "n_TIMESTAMPTZ", "n_DOUBLEPRECISION", "n_BOOLEAN"
     )
 
     val INSERT_SQL = "INSERT INTO data (" +
