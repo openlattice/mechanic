46a6b47f80454dabb2e6cdc671f8e41df28f1412
Katherine Bernstein <katherine@openlattice.com>
2019-07-03 01:32:14 -0700

    idk my  bff jull

--- src/main/kotlin/com/openlattice/mechanic/upgrades/UpgradeEdgesTable.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/UpgradeEdgesTable.kt
@@ -212,8 +212,8 @@ class UpgradeEdgesTable(val toolbox: Toolbox) : Upgrade {
     fun migratedVersionSql(joinColumn: PostgresColumnDefinition, entitySetId: UUID): String {
         return "WITH for_migration AS ( UPDATE ${EDGES.name} SET migrated_version = abs(version) " +
                 "WHERE ${COMPONENT_TYPES.name} = ${IdType.SRC.ordinal} AND ${joinColumn.name} = '$entitySetId' AND " +
-                "(id,edge_comp_1,edge_comp_2,component_types) in ( select id,edge_comp_1,edge_comp_2,component_types FROM ${EDGES.name} WHERE migrated_version < abs(migrated_version)) " +
-                "LIMIT $BATCH_SIZE) ) RETURNING *) "
+                "(id,edge_comp_1,edge_comp_2,component_types) in ( select id,edge_comp_1,edge_comp_2,component_types FROM ${EDGES.name} WHERE migrated_version < abs(migrated_version) " +
+                "LIMIT $BATCH_SIZE) RETURNING *) "
     }
 
     fun addMigratedVersionColumn() {
