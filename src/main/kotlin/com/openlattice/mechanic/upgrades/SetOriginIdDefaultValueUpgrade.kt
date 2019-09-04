e682c3209d3d4ab4c1c78e882b06200cfc807de3
Drew <drew@openlattice.com>
2019-09-04 12:07:34 -0700

    set default value as separate migration

---
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/SetOriginIdDefaultValueUpgrade.kt
@@ -0,0 +1,31 @@
+package com.openlattice.mechanic.upgrades
+
+import com.openlattice.IdConstants
+import com.openlattice.mechanic.Toolbox
+import com.openlattice.postgres.PostgresColumn.ORIGIN_ID
+import com.openlattice.postgres.PostgresTable.DATA
+import org.slf4j.LoggerFactory
+
+
+class SetOriginIdDefaultValueUpgrade(private val toolbox: Toolbox) : Upgrade {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(Toolbox::class.java)
+    }
+
+    override fun getSupportedVersion(): Long {
+        return Version.V2019_08_26.value
+    }
+
+    override fun upgrade(): Boolean {
+
+        val updateDefaultValue = "ALTER TABLE ${DATA.name} ALTER COLUMN ${ORIGIN_ID.name} SET DEFAULT '${IdConstants.EMPTY_ORIGIN_ID.id}'"
+        toolbox.hds.connection.use { conn ->
+            conn.autoCommit = false
+            conn.createStatement().executeUpdate( updateDefaultValue )
+            conn.commit()
+            return true
+        }
+    }
+
+}
