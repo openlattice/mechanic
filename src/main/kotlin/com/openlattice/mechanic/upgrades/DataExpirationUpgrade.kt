114d9ddbf114be4954b03a4b9fdc8b31b02e092d
Piper Keyes <piper@openlattice.com>
2019-10-03 17:07:13 -0700

    actually execute the statement

--- src/main/kotlin/com/openlattice/mechanic/upgrades/DataExpirationUpgrade.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/DataExpirationUpgrade.kt
@@ -8,21 +8,22 @@ import java.sql.Types
 
 class DataExpirationUpgrade(private val toolbox: Toolbox) : Upgrade {
     override fun upgrade(): Boolean {
-        toolbox.hds.connection.use{
+        toolbox.hds.connection.use {
             val stmt = it.prepareStatement(addExpirationColumnsQuery())
+            stmt.execute()
         }
         return true;
     }
 
     override fun getSupportedVersion(): Long {
-        return Version.V2019_07_01.value
+        return Version.V2019_10_03.value
     }
 
     private fun addExpirationColumnsQuery(): String {
         return "ALTER TABLE ${PostgresTable.ENTITY_SETS.name} " +
                 "ADD COLUMN ${PostgresColumn.TIME_TO_EXPIRATION.name} ${PostgresColumn.TIME_TO_EXPIRATION.datatype}, " +
                 "ADD COLUMN ${PostgresColumn.EXPIRATION_BASE_FLAG.name} ${PostgresColumn.EXPIRATION_BASE_FLAG.datatype}, " +
-                "ADD COLUMN ${PostgresColumn.EXPIRATION_DELETE_FLAG.name} ${PostgresColumn.EXPIRATION_DELETE_FLAG.datatype}, "
+                "ADD COLUMN ${PostgresColumn.EXPIRATION_DELETE_FLAG.name} ${PostgresColumn.EXPIRATION_DELETE_FLAG.datatype}, " +
                 "ADD COLUMN ${PostgresColumn.EXPIRATION_START_ID.name} ${PostgresColumn.EXPIRATION_START_ID.datatype}"
     }
 }
