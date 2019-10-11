5665f0197dba23a9dc422a748adea40d8cad5d66
Katherine Bernstein <katherine@openlattice.com>
2019-10-10 18:30:10 -0700

    i broke it

--- src/main/kotlin/com/openlattice/mechanic/pods/MechanicUpgradePod.kt
+++ src/main/kotlin/com/openlattice/mechanic/pods/MechanicUpgradePod.kt
@@ -267,6 +267,7 @@ class MechanicUpgradePod {
     @Bean
     fun addOriginIdToDataPrimaryKey(): AddOriginIdToDataPrimaryKey {
         return AddOriginIdToDataPrimaryKey(toolbox())
+    }
 
     @Bean
     fun insertDeletedChronicleEdgeIds(): InsertDeletedChronicleEdgeIds {
