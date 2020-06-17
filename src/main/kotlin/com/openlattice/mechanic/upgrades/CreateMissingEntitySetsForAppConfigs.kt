113277e3810e80f813be730515775b059fcb59a0
Katherine Bernstein <katherine@openlattice.com>
2020-06-17 12:11:17 -0700

    + grants for org app roles

--- src/main/kotlin/com/openlattice/mechanic/upgrades/CreateMissingEntitySetsForAppConfigs.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/CreateMissingEntitySetsForAppConfigs.kt
@@ -47,11 +47,14 @@ class CreateMissingEntitySetsForAppConfigs(
             val appConfigKeysToCreate = mutableSetOf<AppConfigKey>()
 
             val adminRole = tryGetOrgAdminRole(org)
+            val rolesByPrincipalId = getOrgRolesByPrincipalId(org.id)
             val adminAceKeys = adminRole?.let { listOf(Ace(it, EnumSet.allOf(Permission::class.java))) } ?: listOf()
+            val roleAcesByApp = mutableMapOf<UUID, Set<Ace>>()
 
             org.apps.forEach { appId ->
 
                 val app = apps.getValue(appId)
+                roleAcesByApp[appId] = getRoleAcesForApp(rolesByPrincipalId, org.id, app)
 
                 app.appTypeIds.forEach { appTypeId ->
 
@@ -79,16 +82,13 @@ class CreateMissingEntitySetsForAppConfigs(
             appConfigKeysToCreate.forEach { ack ->
                 val entitySet = generateEntitySet(org, apps.getValue(ack.appId), appTypes.getValue(ack.appTypeId))
                 val entitySetId = entitySetManager.createEntitySet(userOwnerPrincipal, entitySet)
+                val aceKeysToGrant = adminAceKeys + roleAcesByApp.getOrDefault(ack.appId, setOf())
 
                 newAppConfigEntries[ack] = AppTypeSetting(entitySetId, EnumSet.of(Permission.READ, Permission.WRITE))
 
-                if (adminRole == null) {
-                    return@forEach
-                }
-
-                aclsToGrant.add(Acl(AclKey(entitySetId), adminAceKeys))
+                aclsToGrant.add(Acl(AclKey(entitySetId), aceKeysToGrant))
                 toolbox.entityTypes.getValue(appTypes.getValue(ack.appTypeId).entityTypeId).properties.forEach { ptId ->
-                    aclsToGrant.add(Acl(AclKey(entitySetId, ptId), adminAceKeys))
+                    aclsToGrant.add(Acl(AclKey(entitySetId, ptId), aceKeysToGrant))
                 }
 
             }
@@ -101,6 +101,36 @@ class CreateMissingEntitySetsForAppConfigs(
         authManager.addPermissions(aclsToGrant)
 
         return true
+    }
+
+    private fun getOrgRolesByPrincipalId(orgId: UUID): Map<String, SecurablePrincipal> {
+        return spm.getAllRolesInOrganization(orgId).associateBy { it.principal.id }
+    }
+
+    private fun getRoleAcesForApp(rolesByPrincipalId: Map<String, SecurablePrincipal>, orgId: UUID, app: App): Set<Ace> {
+        val aces = mutableSetOf<Ace>()
+        EnumSet.of(Permission.READ, Permission.WRITE, Permission.OWNER).forEach { permission ->
+            val title = "${app.title}-${permission.name}"
+            val principalId = "$orgId-$title"
+
+            var principal: Principal? = null
+
+            if (rolesByPrincipalId.containsKey(principalId)) {
+                principal = rolesByPrincipalId.getValue(principalId).principal
+            } else {
+                rolesByPrincipalId.values.firstOrNull { it.title == title }?.let {
+                    principal = it.principal
+                }
+            }
+
+            if (principal != null) {
+                aces.add(Ace(principal, EnumSet.of(permission)))
+            } else  {
+                logger.info("Could not find $permission principal for app ${app.name} in org $orgId")
+            }
+        }
+
+        return aces
     }
 
     private fun tryGetOrgAdminRole(org: Organization): Principal? {
