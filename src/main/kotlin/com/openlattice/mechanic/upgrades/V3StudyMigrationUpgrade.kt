4df59f09a7957fb535ce0c8dcf3d0dd814da1a55
njhlai <nicholasjhlai@gmail.com>
2022-01-31 20:20:32 -0800

    Added and corrected v2 to v3 property type correspondence

--- src/main/kotlin/com/openlattice/mechanic/upgrades/V3StudyMigrationUpgrade.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/V3StudyMigrationUpgrade.kt
@@ -52,13 +52,17 @@ class V3StudyMigrationUpgrade(
 
         // Property Types of ol.study
         val studiesPropertyTypes = propertyTypes.entrySet(
-            Predicates.`in`<UUID, PropertyType>(PropertyTypeMapstore.ID_INDEX,
-                                                UUID.fromString("70d2ff1c-2450-4a47-a954-a7641b7399ae"), // general.fullname
-                                                UUID.fromString("c502df5b-ec63-4f15-b529-d38695366c75"), // diagnosis.Description
-                                                UUID.fromString("06083695-aebe-4a56-9b98-da6013e93a5e"), // location.latitude
-                                                UUID.fromString("e8f9026a-2494-4749-84bb-1499cb7f215c"), // location.longitude
-                                                UUID.fromString("0011bbfe-d5d4-4f88-97a8-cdeb821deb6f") //ol.version
-                                                )
+            Predicates.`in`<UUID, PropertyType>(
+                PropertyTypeMapstore.ID_INDEX,
+                UUID.fromString("ee3a7573-aa70-4afb-814d-3fad27cda988"), // general.stringid
+                UUID.fromString("70d2ff1c-2450-4a47-a954-a7641b7399ae"), // general.fullname
+                UUID.fromString("c502df5b-ec63-4f15-b529-d38695366c75"), // diagnosis.Description
+                UUID.fromString("06083695-aebe-4a56-9b98-da6013e93a5e"), // location.latitude
+                UUID.fromString("e8f9026a-2494-4749-84bb-1499cb7f215c"), // location.longitude
+                UUID.fromString("54f64f11-9c4d-4701-897b-630ffeb94a21"), // sharing.name
+                UUID.fromString("0011bbfe-d5d4-4f88-97a8-cdeb821deb6f"), // ol.version
+                UUID.fromString("6a1f7cf6-80eb-4fe9-a9f4-49cad15c6154") // contact.Email
+            )
         )
 
         entitySets.entrySet(
@@ -87,12 +91,14 @@ class V3StudyMigrationUpgrade(
                         studyService.createStudy(
                             connection,
                             Study(
-                                studyId = id,
+                                studyId = FQNtoValue[EdmConstants.STRING_ID_FQN],
                                 title = FQNtoValue[EdmConstants.FULL_NAME_FQN],
                                 description = FQNtoValue[FullQualifiedName("diagnosis.Description")],
                                 lat = FQNtoValue[FullQualifiedName("location.latitude")],
                                 lon = FQNtoValue[FullQualifiedName("location.longitude")],
+                                group = FQNtoValue[FullQualifiedName("sharing.name")]
                                 version = FQNtoValue[EdmConstants.VERSION_FQN],
+                                contact = FQNtoValue[FullQualifiedName("contact.Email")]
                                 organizationIds = setOf(orgId)
                             )
                         )
