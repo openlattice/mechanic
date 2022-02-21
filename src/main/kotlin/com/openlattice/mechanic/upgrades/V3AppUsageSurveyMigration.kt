720ccbf7f8ad4137f9058094eb0194563134c9e2
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-02-21 12:50:47 -0800

    fix user id

--- src/main/kotlin/com/openlattice/mechanic/upgrades/V3AppUsageSurveyMigration.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/V3AppUsageSurveyMigration.kt
@@ -44,7 +44,7 @@ class V3AppUsageSurveyMigration(
     private val allSurveyDataByParticipantEKID: MutableMap<UUID, List<Map<FullQualifiedName, Set<Any?>>>> = mutableMapOf()
 
     //TODO: replace empty strings with chronicle super user ids (auth0 and google-oauth2) when running migration
-    private val chronicleSuperUserIds = setOf(" auth0|5ae9026c04eb0b243f1d2bb6", "google-oauth2|113860246540203337319")
+    private val chronicleSuperUserIds = setOf("auth0|5ae9026c04eb0b243f1d2bb6", "google-oauth2|113860246540203337319")
 
     companion object {
         private val DATA_COLLECTION_APP_ID = UUID.fromString("c4e6d8fd-daf9-41e7-8c59-2a12c7ee0857")
