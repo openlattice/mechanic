fc15729d8fe79a6f0f477ca87e17d8890ca7eae1
Andrew Carter <and.s.carter@gmail.com>
2022-02-22 19:06:11 -0800

    only pull v2 chronicle orgs and add migration summary measures

--- src/main/kotlin/com/openlattice/mechanic/upgrades/V3TimeUseDiaryUpgrade.kt
+++ src/main/kotlin/com/openlattice/mechanic/upgrades/V3TimeUseDiaryUpgrade.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.geekbeast.rhizome.configuration.RhizomeConfiguration
-import com.hazelcast.query.Predicates
 import com.openlattice.authorization.Principal
 import com.openlattice.data.requests.NeighborEntityDetails
 import com.openlattice.data.storage.MetadataOption
@@ -15,7 +14,6 @@ import com.openlattice.graph.PagedNeighborRequest
 import com.openlattice.hazelcast.HazelcastMap
 import com.openlattice.mechanic.Toolbox
 import com.openlattice.organizations.roles.HazelcastPrincipalService
-import com.openlattice.postgres.mapstores.EntitySetMapstore
 import com.openlattice.search.SearchService
 import com.openlattice.search.requests.EntityNeighborsFilter
 import com.zaxxer.hikari.HikariConfig
@@ -24,10 +22,7 @@ import org.apache.olingo.commons.api.edm.FullQualifiedName
 import org.slf4j.LoggerFactory
 import java.sql.Types
 import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
-import java.time.temporal.TemporalAccessor
 import java.util.*
-import kotlin.NoSuchElementException
 
 /**
  * @author Andrew Carter andrew@openlattice.com
@@ -46,10 +41,10 @@ class V3TimeUseDiaryUpgrade(
     ) : Upgrade {
 
     private val propertyTypes = HazelcastMap.PROPERTY_TYPES.getMap(toolbox.hazelcast)
-    private val entitySets = HazelcastMap.ENTITY_SETS.getMap(toolbox.hazelcast)
     private val entitySetIds = HazelcastMap.ENTITY_SETS.getMap(toolbox.hazelcast).values.associateBy { it.name }
     private val organizations = HazelcastMap.ORGANIZATIONS.getMap(toolbox.hazelcast)
     private val answerPropertyTypeIds = propertyTypes.getAll(setOf(OL_VALUES_ID, OL_ID_ID))
+    private val appConfigs = HazelcastMap.APP_CONFIGS.getMap(toolbox.hazelcast)
     // TODO: replace empty strings with chronicle super user ids (auth0 and google-oauth2) when running migration
     private val chronicleSuperUserIds = setOf("")
 
@@ -59,6 +54,13 @@ class V3TimeUseDiaryUpgrade(
         val objectMapper = ObjectMapper().registerModule( JavaTimeModule() )
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
+        var answersRead = 0
+        var answersWritten = 0
+        var submissionsRead = 0
+        var submissionsWritten = 0
+        var answersSkipped = 0
+
+        private val CHRONICLE_SURVEYS_APP_ID = UUID.fromString("bb44218b-515a-4314-b955-df2c991b2575")
         private val COMPLETED_DT_FQN = FullQualifiedName("date.completeddatetime")
         private val SUBJECT_ID_FQN = FullQualifiedName("nc.SubjectIdentification")
         private val OL_TITLE_FQN = FullQualifiedName("ol.title")
@@ -69,10 +71,12 @@ class V3TimeUseDiaryUpgrade(
         private val OL_VALUES_ID = UUID.fromString("dcc3bc24-3a5d-45cf-8e38-bc9ba8c43d06")
         private val OL_ID_ID = UUID.fromString("39e13db7-a730-421a-a600-ae0674060140")
         private val OL_STUDY_ID = UUID.fromString("80c86a96-0e3f-46eb-9fbb-60d9174566a5")
-        private val answerEntityTypeId = UUID.fromString("7912f235-1959-4dd2-8a61-d85cd09b0c34")
-        private val participantEntitySetSuffix = "participants"
-        private val studyEntitySetSuffix = "studies"
-        private val participatedInEntitySetSuffix = "participatedin"
+        private const val PARTICIPANT_ENTITY_SET_SUFFIX = "participants"
+        private const val STUDY_ENTITY_SET_SUFFIX = "studies"
+        private const val PARTICIPATED_IN_ENTITY_SET_SUFFIX = "participatedin"
+        private const val ANSWER_ENTITY_SET_SUFFIX = "answer"
+        private const val CHRONICLE_ENTITY_SET_PREFIX = "chronicle_"
+        private const val CHRONICLE_SURVEYS_ENTITY_SET_PREFIX = "chronicle_surveys_"
 
         private val dropExistingTimeUseDiaryTableSql = """
             DROP TABLE IF EXISTS public.time_use_diary_submissions;
@@ -120,27 +124,34 @@ class V3TimeUseDiaryUpgrade(
     override fun upgrade(): Boolean {
         logger.info("Starting migration of Time Use Diaries to table 'time_use_diary_submissions'")
         // Get all answer entity sets from EntitySets Map
-        val answerEtySetsByOrganizationId = getAllAnswerEntitySets().groupBy { it.value.organizationId }
         val superUserPrincipals = getChronicleSuperUserPrincipals()
 
-        answerEtySetsByOrganizationId.forEach { (organizationId, answerEntitySets) ->
-            // Prepare EntitySetIds and Authorized Property Types
-            val entityKeyIdsByEntitySetIds = answerEntitySets.associate { it.key to Optional.of(setOf<UUID>()) }.toMap()
-            val answerEntitySetIds = entityKeyIdsByEntitySetIds.keys
-            val answerAuthPropertyTypes = answerEntitySets.associate { it.key to answerPropertyTypeIds }.toMap()
+        val v2ChronicleSurveyOrganizations = appConfigs.keys
+            .filter { it.appId == CHRONICLE_SURVEYS_APP_ID }
+            .map { it.organizationId }
+            .toMutableSet()
+
+        v2ChronicleSurveyOrganizations.forEach{ organizationId ->
+            logger.info("Gathering answers for organization $organizationId")
+
+            val answerEntitySet = getEntitySetForOrg(CHRONICLE_SURVEYS_ENTITY_SET_PREFIX, organizationId, ANSWER_ENTITY_SET_SUFFIX)
+            val entityKeyIdsByEntitySetIds = mapOf(answerEntitySet.id to Optional.of(setOf<UUID>()))
+            val answerAuthPropertyTypes = mapOf(answerEntitySet.id to answerPropertyTypeIds)
             val answerNeighborhoods = mutableListOf<AnswerNeighborhood>()
 
-            // Use PostgresEntityDataQueryService to pull all answer entities
-            val answerEntitiesByAnswerId = getAllAnswerEntities(entityKeyIdsByEntitySetIds, answerAuthPropertyTypes)
+            // Use PostgresEntityDataQueryService to pull answer entities for organization
+            val answerEntitiesByAnswerId = getAnswerEntities(entityKeyIdsByEntitySetIds, answerAuthPropertyTypes)
 
             // Gather each answer neighborhood using SearchService
             for ((answerId, fqnToValue) in answerEntitiesByAnswerId) {
                 logger.info("Searching for neighbors of answer $answerId")
+                answersRead += 1
+
                 val adminRoleAclKey = organizations[organizationId]?.adminRoleAclKey
                 val principals = principalService.getAllUsersWithPrincipal(adminRoleAclKey!!).map { it.principal }.toSet() + superUserPrincipals
 
                 val filter = EntityNeighborsFilter(setOf(answerId))
-                val searchResult = searchForNeighbors(answerId, answerEntitySetIds, filter, principals)
+                val searchResult = searchForNeighbors(answerId, setOf(answerEntitySet.id), filter, principals)
 
                 val answerNeighborhood = extractAnswerNeighborhoodFromSearchResult(searchResult)
                 answerNeighborhood.organizationId = organizationId
@@ -148,14 +159,15 @@ class V3TimeUseDiaryUpgrade(
 
                 if (answerNeighborhood.participantESID == null || answerNeighborhood.participantId == null) {
                     logger.warn("Skipping migration for participant $answerId. Failed to retrieve associated participant details.")
+                    answersSkipped += 1
                     continue
                 }
 
                 val participantFilter = EntityNeighborsFilter(
                     setOf(answerNeighborhood.participantId),
-                    Optional.of(setOf(getESIDForOrg(organizationId, participantEntitySetSuffix))),
-                    Optional.of(setOf(getESIDForOrg(organizationId, studyEntitySetSuffix))),
-                    Optional.of(setOf(getESIDForOrg(organizationId, participatedInEntitySetSuffix)))
+                    Optional.of(setOf(getEntitySetForOrg(CHRONICLE_ENTITY_SET_PREFIX, organizationId, PARTICIPANT_ENTITY_SET_SUFFIX).id)),
+                    Optional.of(setOf(getEntitySetForOrg(CHRONICLE_ENTITY_SET_PREFIX, organizationId, STUDY_ENTITY_SET_SUFFIX).id)),
+                    Optional.of(setOf(getEntitySetForOrg(CHRONICLE_ENTITY_SET_PREFIX, organizationId, PARTICIPATED_IN_ENTITY_SET_SUFFIX).id))
                 )
                 val participantNeighbors = searchForNeighbors(
                     answerNeighborhood.participantId,
@@ -165,18 +177,25 @@ class V3TimeUseDiaryUpgrade(
                 )
 
                 val study = participantNeighbors.filter { it.neighborEntitySet.get().entityTypeId == OL_STUDY_ID }
-                answerNeighborhood.studyId = study.first().neighborId.get() // Only one study should be returned
+                answerNeighborhood.studyId = study.first().neighborId.get() // study var should contain only a single study
 
                 answerNeighborhoods.add(answerNeighborhood)
             }
 
             // Create Submission objects containing all questions and answers submitted as part of that survey
             val answersBySubmissionId = answerNeighborhoods.groupBy { it.submissionId }
+            submissionsRead += answersBySubmissionId.keys.size
             val submissions = createSubmissionsFromAnswerNeighborhoods(answersBySubmissionId)
 
             // Insert all submissions into postgres
             insertSubmissions(submissions)
         }
+        logger.info("Finished migration. Migration details:")
+        logger.info("Answers read:        $answersRead")
+        logger.info("Answers written:     $answersWritten")
+        logger.info("Answers skipped:     $answersSkipped")
+        logger.info("Submissions read:    $submissionsRead")
+        logger.info("Submissions written: $submissionsWritten")
         return true
     }
 
@@ -190,6 +209,7 @@ class V3TimeUseDiaryUpgrade(
      * 6) TimeUseDiaryResponse
      */
     private fun insertSubmissions(submissions: List<Submission>) {
+        var answersInBatch = 0
         val (hikariConfiguration) = rhizomeConfiguration.datasourceConfigurations["alpr"]!!
         val hc = HikariConfig(hikariConfiguration)
         val hds = HikariDataSource(hc)
@@ -205,27 +225,23 @@ class V3TimeUseDiaryUpgrade(
                     preparedStatement.setObject(index++, submission.submissionDate, Types.TIMESTAMP_WITH_TIMEZONE)
                     preparedStatement.setObject(index, objectMapper.writeValueAsString(submission.submission), Types.OTHER)
                     preparedStatement.addBatch()
+                    answersInBatch += submission.submission.size
                 }
                 logger.info("Executing batch of submission inserts")
-                preparedStatement.executeBatch()
+                submissionsWritten += preparedStatement.executeBatch().sum()
+                answersWritten += answersInBatch
             } catch (ex: Exception) {
                 logger.error("Error inserting submissions $ex")
             }
         }
     }
 
-    private fun getAllAnswerEntitySets(): Set<Map.Entry<UUID, EntitySet>> {
-        return entitySets.entrySet(
-            Predicates.equal(EntitySetMapstore.ENTITY_TYPE_ID_INDEX, answerEntityTypeId)
-        ) ?: throw NoSuchElementException("No answer entities found.. Aborting")
+    private fun getEntitySetForOrg(prefix: String, organizationId: UUID, entityTypeName: String): EntitySet {
+        val entitySetNameByTemplate = "$prefix${organizationId.toString().replace("-", "")}_$entityTypeName"
+        return entitySetIds.getValue(entitySetNameByTemplate)
     }
 
-    private fun getESIDForOrg(organizationId: UUID, entityTypeName: String): UUID {
-        val entitySetNameByTemplate = "chronicle_${organizationId.toString().replace("-", "")}_$entityTypeName"
-        return entitySetIds.getValue(entitySetNameByTemplate).id
-    }
-
-    private fun getAllAnswerEntities(
+    private fun getAnswerEntities(
         answerEntityKeyIds: Map<UUID, Optional<Set<UUID>>>,
         answerAuthPropertyTypes: Map<UUID, MutableMap<UUID, PropertyType>>
     ): Map<UUID, MutableMap<FullQualifiedName, MutableSet<Any>>> {
@@ -299,7 +315,7 @@ class V3TimeUseDiaryUpgrade(
             null,
             null,
             participantName,
-            submissionDateTime!!,
+            submissionDateTime,
             question,
             questionCode,
             null,
@@ -324,6 +340,7 @@ class V3TimeUseDiaryUpgrade(
             for (answer in answers) {
                 if (answer.questionString == null || answer.answer == null || answer.questionCode == null) {
                     logger.warn("Skipping TimeUseDiaryResponse for answer $answer. Failed to retrieve required question or answer details.")
+                    answersSkipped += 1
                     continue
                 }
                 submissionResponses.add(
